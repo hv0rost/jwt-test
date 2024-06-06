43b086a339f6b706593ae5a79d03701814f783df
Daniil Matyash <daniil.matyash@aisa.ru>
2024-06-06 15:14:41 +0300

    add current route display

--- src/components/RouteItems.vue
+++ src/components/RouteItems.vue
@@ -1,34 +1,48 @@
 <script setup lang="ts">
 import { ComputedRef, computed } from "vue"
-
-import { useUsersStore } from "../store/user"
+import { useRoute } from "vue-router"
 import { storeToRefs } from "pinia"
 
+import { useUsersStore } from "../store/user"
 const { permissions } = storeToRefs(useUsersStore())
+
+const router = useRoute()
 
 const routes: ComputedRef = computed(() => [
   {
     name: "page-one",
     path: "/page-one",
     showed: true,
+    selected: router.name === "PageOne",
   },
   {
     name: "page-two",
     path: "/page-two",
     showed: permissions?.value?.pageTwo,
+    selected: router.name === "PageTwo",
   },
   {
     name: "page-three",
     path: "/page-three",
     showed: permissions?.value?.pageThree,
+    selected: router.name === "PageThree",
   },
 ])
 </script>
 
 <template>
   <div class="nav">
-    <div v-for="route in routes" :key="route.name" style="display: flex; justify-content: space-around;">
-      <div v-if="route.showed" class="nav-item" @click="$router.push(route.path)">
+    <div
+      v-for="route in routes"
+      :key="route.name"
+      style="display: flex; justify-content: space-around"
+    >
+      <div
+        v-if="route.showed"
+        :style="route.selected ? 'background-color: #f1f1f1' : ''"
+        class="nav-item"
+        @click="$router.push(route.path)"
+      >
         {{ route.name }}
       </div>
     </div>
